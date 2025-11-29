67aad39e3e4a5bf6e718dd7c1193dffef4a134b6
root <root@localhost.localdomain>
2025-11-29 18:26:43 +0000

    Final version before cloud build

---
+++ react_native_app/src/api/config.js
@@ -0,0 +1,1 @@
+export const BASE_URL = 'http://YOUR_BACKEND_URL:5000';
---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,1 @@
+import React from 'react'; import { View, Text } from 'react-native'; export default ()=> (<View><Text>Login</Text></View>);
